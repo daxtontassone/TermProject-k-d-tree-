5658422c967543d54e6ea4a2cc473fa2cd4b4f02
daxtontassone <90425113+daxtontassone@users.noreply.github.com>
2023-04-24 23:03:54 -0400

    Update kdtree.h

--- kdtree.h
+++ kdtree.h
@@ -2,18 +2,30 @@
 
 class KDTree {
 private:
+    //root of tree
     Node* root;
+    //size, which is the k dimensions
     int size;
-    bool same(int point1[], int point2[]);
+    //function that returns the median of a 2d vector
     int find_median(std::vector<std::vector<double>> list, int dimension, int &median_index);
+    //construct fuction
     Node* construct(std::vector<std::vector<double>> list, int depth);
+    //function that performs search
     int search_helper(Node* root, std::vector<double> point, unsigned depth);
+    //function that performs insert
     void insert_helper(Node* root, std::vector<double> point, int depth);
+    //function that performs print
     void print_helper(Node* node, int depth);
 public:
+    //constructors
     KDTree(std::vector<std::vector<double>> list, int size);
     KDTree();
+    //public insert function
     void insert(std::vector<double> point);
+    //public search function
     int search(std::vector<double> point);
+    //public print function
     void print();
 };
+
+
