e9a0378be8b8fd99627d4034cdb2e0380899a871
daxtontassone <90425113+daxtontassone@users.noreply.github.com>
2023-04-24 20:30:22 -0400

    Update kdtree.h

--- kdtree.h
+++ kdtree.h
@@ -3,8 +3,11 @@
 class KDTree {
 private:
     bool same(int point1[], int point2[]);
-
+    int find_median(std::vector<std::vector<double>> list, int dimension);
+    Node* construct(std::vector<std::vector<double>> list, int depth, int i);
+    int size;
 public:
+    KDTree(std::vector<std::vector<double>> list, int size);
     Node* insert(Node *root, int point[], unsigned depth);
     int search(Node* root, int point[], unsigned depth);
     void print();
